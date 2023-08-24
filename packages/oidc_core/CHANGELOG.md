3f51848ba654cd87418779e0f6d5dd14da61f98d
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2023-08-25 00:40:45 +0300

    published oidc_core

--- packages/oidc_core/CHANGELOG.md
+++ packages/oidc_core/CHANGELOG.md
@@ -1,3 +1,7 @@
-## 1.0.0
+## 0.1.0+1
+
+- Added topics.
+
+## 0.1.0
 
 - Initial version.
--- packages/oidc_core/example/oidc_core_example.dart
+++ packages/oidc_core/example/oidc_core_example.dart
@@ -1,6 +1,13 @@
-import 'package:oidc_core/oidc_core.dart';
+import 'dart:convert';
 
-void main() {
-  // var awesome = Oidc();
-  // print('awesome: ${awesome.isAwesome}');
+import 'package:oidc_core/oidc_core.dart';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  final googleUri =
+      Uri.parse('https://accounts.google.com/.well-known/openid-configuration');
+  final googleConfigResp = await http.get(googleUri);
+  final parsedJson = jsonDecode(googleConfigResp.body);
+  final parsedMetadata = OidcProviderMetadata.fromJson(parsedJson);
+  print(parsedMetadata.authorizationEndpoint);
 }
