93bbf927efaccf2009e217531193d3f7e594af41
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2023-08-28 17:50:48 +0300

    attempt

--- packages/oidc_core/test/e2e_test.dart
+++ packages/oidc_core/test/e2e_test.dart
@@ -2,18 +2,26 @@ import 'package:http/http.dart';
 import 'package:oidc_core/oidc_core.dart';
 import 'package:test/test.dart';
 
-void main() {
+void main() async {
+  final serverUri = Uri.parse('http://localhost:4011');
+  final wellKnownUrl = OidcUtils.getWellKnownUriFromBase(serverUri);
+  try {
+    final config = await OidcUtils.getConfiguration(wellKnownUrl);
+    // print(config);
+    expect(config.issuer.toString(), serverUri.toString());
+  } on ClientException {
+    print("Skipping e2e tests since server isn't up");
+    test('dummy', () {
+      
+    });
+    return;
+  }
+
   group('E2E', () {
     test('fetch discovery', () async {
-      final url =
-          Uri.parse('http://localhost:4011/.well-known/openid-configuration');
-      try {
-        final config = await OidcUtils.getConfiguration(url);
-        // print(config);
-        expect(config.issuer.toString(), 'http://localhost:4011');
-      } on ClientException {
-        print("Skipping test since server isn't up");
-      }
+      final config = await OidcUtils.getConfiguration(wellKnownUrl);
+      // print(config);
+      expect(config.issuer.toString(), 'http://localhost:4011');
     });
   });
 }
