b7984978049f20c9defa449185ef5963d6ee0bd1
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2023-09-04 11:12:29 +0300

    feat: added more helpers

---
+++ packages/oidc/lib/src/models/user.dart
@@ -0,0 +1,4 @@
+/// A user is a verified JWT id_token, with some metadata.
+class OidcUser {
+  
+}
--- packages/oidc_core/lib/src/endpoints/_exports.dart
+++ packages/oidc_core/lib/src/endpoints/_exports.dart
@@ -1,4 +1,4 @@
 export 'authorize/_exports.dart';
-export 'oidc_discovery/_exports.dart';
+export 'discovery/_exports.dart';
 export 'par/_exports.dart';
 export 'token/_exports.dart';
