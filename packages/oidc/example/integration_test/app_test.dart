4aae3302d6f9d5196978ab8cc34235a1422078eb
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2023-09-09 17:49:28 +0300

    add initial integration test

--- packages/oidc/example/integration_test/app_test.dart
+++ packages/oidc/example/integration_test/app_test.dart
@@ -2,20 +2,21 @@ import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-
+import 'package:oidc_example/app_state.dart' as app_state;
 import 'package:oidc_example/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('E2E', () {
-    testWidgets('getPlatformName', (tester) async {
+    testWidgets('manager initializes correctly', (tester) async {
       app.main();
       await tester.pumpAndSettle();
-      await tester.tap(find.text('Get Platform Name'));
-      await tester.pumpAndSettle();
-      final expected = expectedPlatformName();
-      await tester.ensureVisible(find.text('Platform Name: $expected'));
+      expect(app_state.manager.didInit, true);
+      // await tester.tap(find.text('Get Platform Name'));
+      // await tester.pumpAndSettle();
+      // final expected = expectedPlatformName();
+      // await tester.ensureVisible(find.text('Platform Name: $expected'));
     });
   });
 }
