6099287290cb4bfb253d00e310f77558b3c80049
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2023-09-12 10:51:41 +0300

    export oidc_core for simplicity

--- packages/oidc/lib/oidc.dart
+++ packages/oidc/lib/oidc.dart
@@ -1,5 +1,7 @@
 // import 'package:oidc_platform_interface/oidc_platform_interface.dart';
 export 'package:jose/jose.dart';
+// ignore: invalid_export_of_internal_element
+export 'package:oidc_core/oidc_core.dart';
 export 'package:oidc_platform_interface/oidc_platform_interface.dart';
 
 export 'src/_exports.dart';
