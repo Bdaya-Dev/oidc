0132918e172b838e37a7b2c02738adcccd6c88e2
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2023-09-16 07:47:08 +0300

    format

--- packages/oidc_platform_interface/lib/src/platform.dart
+++ packages/oidc_platform_interface/lib/src/platform.dart
@@ -65,7 +65,7 @@ class NoOpOidcPlatform extends OidcPlatform {
   ) {
     throw UnimplementedError();
   }
-  
+
   @override
   Future<OidcEndSessionResponse?> getEndSessionResponse(
     OidcProviderMetadata metadata,
