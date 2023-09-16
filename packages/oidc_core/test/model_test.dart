2f868b4d658e0145789ef301ea5ca07e198d5fc7
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2023-09-16 19:59:58 +0300

    fix test

--- packages/oidc_core/test/model_test.dart
+++ packages/oidc_core/test/model_test.dart
@@ -94,7 +94,7 @@ void main() {
                   createdAt.toIso8601String(),
             };
             final obj = OidcToken.fromJson(src);
-            expect(obj.isOidc, true);
+            expect(obj.isOidc, false);
             expect(obj.toJson(), src);
             expect(obj.expiresIn, expiresIn);
             expect(
