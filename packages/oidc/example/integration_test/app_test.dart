aeaee80ff2c8542956260fb2c6830a493cce3f0f
ahmednfwela <ahmednfwela@bdaya-dev.com>
2024-11-26 05:37:05 +0200

    remove dart:io reference

--- packages/oidc/example/integration_test/app_test.dart
+++ packages/oidc/example/integration_test/app_test.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:oidc_example/app_state.dart' as app_state;
