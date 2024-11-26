a6dc02f2240a655b54e0f9dd398c708e30cf86f2
ahmednfwela <ahmednfwela@bdaya-dev.com>
2024-11-26 04:36:43 +0200

    attempt debugging in the app.

--- packages/oidc/example/integration_test/app_test.dart
+++ packages/oidc/example/integration_test/app_test.dart
@@ -4,7 +4,19 @@ import 'package:oidc_example/app_state.dart' as app_state;
 import 'package:oidc_example/main.dart' as example;
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  tearDownAll(
+    () async {
+      print('tearDownAll is called.');
+      IntegrationTestWidgetsFlutterBinding.instance.inTest;
+      if (await binding.allTestsPassed.future) {
+        print('tearDownAll is called AND allTestsPassed = true.');
+      } else {
+        print('tearDownAll is called AND allTestsPassed = false.');
+      }      
+    },
+  );
 
   group('E2E', () {
     testWidgets('manager initializes correctly', (tester) async {
