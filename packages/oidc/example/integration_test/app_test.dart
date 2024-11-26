85c1d27ef2fa0e478e4b972ae525519e411537c7
ahmednfwela <ahmednfwela@bdaya-dev.com>
2024-11-26 06:26:36 +0200

    chore: do some cleanup for web integration tests

--- packages/oidc/example/integration_test/app_test.dart
+++ packages/oidc/example/integration_test/app_test.dart
@@ -4,29 +4,7 @@ import 'package:oidc_example/app_state.dart' as app_state;
 import 'package:oidc_example/main.dart' as example;
 
 void main() {
-  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  tearDownAll(
-    () async {
-      // under normal circumstances this callback should NEVER be called, since
-      // IntegrationTestWidgetsFlutterBinding will shutdown the test process before this is reached.
-
-      // however when running integration tests on macos, web, this might not happen and tests will hang forever.
-      print(
-        'tearDownAll is called, binding.allTestsPassed.isCompleted: ${binding.allTestsPassed.isCompleted}, binding.failureMethodsDetails.isEmpty: ${binding.failureMethodsDetails.isEmpty}.',
-      );
-      if (!binding.allTestsPassed.isCompleted) {
-        print('binding.allTestsPassed was not completed, completing it...');
-
-        binding.allTestsPassed.complete(binding.failureMethodsDetails.isEmpty);
-      }
-
-      print(
-          'binding.allTestsPassed = ${await binding.allTestsPassed.future}, will attempt to call  binding.callbackManager.cleanup().');
-      binding.callbackManager.cleanup();
-      print('binding.callbackManager.cleanup() finished.');
-    },
-  );
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('E2E', () {
     testWidgets('manager initializes correctly', (tester) async {
